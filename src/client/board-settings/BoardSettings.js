6d21facf7897e60877cd1b3d4befc60e7fce8dff
AnnaShar <sharuntsova.anna@yandex.ru>
2021-11-13 16:26:46 +0300

    Trying to close settings on outside click. Not working. Problem with click on setting icon

--- src/client/board-settings/BoardSettings.js
+++ src/client/board-settings/BoardSettings.js
@@ -1,61 +1,91 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useEffect} from 'react';
 import {UserSettingsContext} from '../context-store/user-settings-context.js';
 import {CirclePicker} from 'react-color';
-import Select, { StylesConfig } from 'react-select'
+import Select from 'react-select';
+import {useDetectClickOutside} from "react-detect-click-outside";
 import themes from '../constants/themes.js';
 import languages from '../constants/languages.js';
 import texts from '../constants/texts.js';
 import './BoardSettings.css';
 
+
 export const BoardSettings = () => {
-    const {theme, setTheme, language, setLanguage} = useContext(UserSettingsContext);
+    const {theme, setTheme, language, setLanguage, settingsIsOpen, openSettings} = useContext(UserSettingsContext);
+    const settingsContainer = useRef(null);
+    // const closeToggle = () => {
+    //     openSettings(false)
+    // }
+    // const ref = useDetectClickOutside({onTriggered: closeToggle});
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClick);
+        return () => {
+            document.removeEventListener("mousedown", handleClick);
+        };
+    }, []);
 
     const handleColorChange = (color) => {
-        console.log(color.hex);
         setTheme(themes[color.hex]);
-        console.log(themes[color.hex]);
     }
     const handleLanguageChange = (language) => {
         setLanguage(languages[language.value]);
     }
 
-    return (
-        <div className='board__settings'>
-            <h2>{texts.settings.header[language.value]}</h2>
-            <div className='setting-item theme-settings'>
-                <div className='setting-item__header'>
-                    {texts.settings.chooseTheme[language.value]}
-                </div>
-                <div className='setting-item__body'>
-                    <CirclePicker
-                        color={theme.base}
-                        colors={Object.keys(themes)}
-                        onChange={handleColorChange}
-                    />
-                </div>
-            </div>
+    const handleClick = (e) => {
+        if (settingsContainer.current && !settingsContainer.current.contains(e.target)
+        && e.target.id!=='setting-button') {
+            openSettings(false);
+        }
+        // console.log(e.target)
+        if (e.target.id==='setting-button'){
+            console.log('here')
+        }
+    }
 
-            <div className='setting-item theme-settings'>
-                <div className='setting-item__header'>
-                    {texts.settings.chooseLanguage[language.value]}
+    return (
+        <>
+            {settingsIsOpen &&
+            <div
+                // ref={ref}
+                className='board__settings'
+            >
+                <h2>{texts.settings.header[language.value]}</h2>
+                <div className='setting-item theme-settings'>
+                    <div className='setting-item__header'>
+                        {texts.settings.chooseTheme[language.value]}
+                    </div>
+                    <div className='setting-item__body'>
+                        <CirclePicker
+                            color={theme.base}
+                            colors={Object.keys(themes)}
+                            onChange={handleColorChange}
+                        />
+                    </div>
                 </div>
-                <div className='setting-item__body'>
-                    <Select
-                        defaultValue = {language}
-                        options={Object.values(languages)}
-                        onChange={handleLanguageChange}
-                        theme={(themeSelect) => ({
-                            ...themeSelect,
-                            colors: {
-                                ...themeSelect.colors,
-                                primary50: theme.light,
-                                primary25: theme.light,
-                                primary: theme.base,
-                            },
-                        })}
-                    />
+
+                <div className='setting-item theme-settings'>
+                    <div className='setting-item__header'>
+                        {texts.settings.chooseLanguage[language.value]}
+                    </div>
+                    <div className='setting-item__body'>
+                        <Select
+                            defaultValue={language}
+                            options={Object.values(languages)}
+                            onChange={handleLanguageChange}
+                            theme={(themeSelect) => ({
+                                ...themeSelect,
+                                colors: {
+                                    ...themeSelect.colors,
+                                    primary50: theme.light,
+                                    primary25: theme.light,
+                                    primary: theme.base,
+                                },
+                            })}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            }
+        </>
     )
 }
