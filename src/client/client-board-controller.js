a4e7b3c3284e82c72c419fd4c9199886e3288a67
AnnaShar <sharuntsova.anna@yandex.ru>
2021-12-07 18:51:59 +0300

    Refactor code

--- src/client/client-board-controller.js
+++ src/client/client-board-controller.js
@@ -4,10 +4,10 @@ import {showErrorMessage} from './utils/notifier.js';
 import texts from './constants/texts.js';
 import history from './history.js';
 
-const handleRequest = async (handler) => {
-    showErrorMessage(texts.errorMessages.default);
+
+const handleRequest = async (handler, ...requestParams) => {
     try {
-        return await handler();
+        return await handler(...requestParams);
     } catch (e) {
         showErrorMessage(texts.errorMessages.default);
     }
@@ -21,57 +21,40 @@ const getBoard = async () => {
     }
 }
 
-const addColumn = async (columnName) => {
-    return handleRequest(async () => {
-        return await serverRequest.addColumn(columnName);
-    });
+const addColumn = (columnName) => {
+    return handleRequest(serverRequest.addColumn, columnName);
 }
 
-const addTask = async (task, columnID) => {
-    return handleRequest(async () => {
-        return await serverRequest.addTask(task, columnID);
-    });
+const addTask = (task, columnID) => {
+    return handleRequest(serverRequest.addTask, task, columnID);
 }
 
-const moveTask = async (taskID, source, destination) => {
-    return handleRequest(async () => {
-        return await serverRequest.moveTask(taskID, source, destination);
-    });
+const moveTask = (taskID, source, destination) => {
+    return handleRequest(serverRequest.moveTask, taskID, source, destination);
 }
 
-const deleteTask = async (taskID, columnID) => {
-    return handleRequest(async () => {
-        return await serverRequest.deleteTask(taskID, columnID);
-    });
+const deleteTask = (taskID, columnID) => {
+    return handleRequest(serverRequest.deleteTask, taskID, columnID);
 }
 
-const moveColumn = async (columnID, sourceIndex, destinationIndex) => {
-    return handleRequest(async () => {
-        return await serverRequest.moveColumn(columnID, sourceIndex, destinationIndex);
-    });
+const moveColumn = (columnID, sourceIndex, destinationIndex) => {
+    return handleRequest(serverRequest.moveColumn, columnID, sourceIndex, destinationIndex);
 }
 
-const deleteColumn = async (columnID) => {
-    return handleRequest(async () => {
-        return await serverRequest.deleteColumn(columnID);
-    });
+const deleteColumn = (columnID) => {
+    return handleRequest(serverRequest.deleteColumn, columnID);
 }
 
-const changeColumnName = async (columnID, columnName) => {
-    return handleRequest(async () => {
-        return await serverRequest.changeColumnName(columnID, columnName);
-    });
+const changeColumnName = (columnID, columnName) => {
+    return handleRequest(serverRequest.changeColumnName, columnID, columnName);
 }
 
-const changeBoardName = async (boardName) => {
-    return handleRequest(async () => {
-        return await serverRequest.changeBoardName(boardName);
-    });
+const changeBoardName = (boardName) => {
+    return handleRequest(serverRequest.changeBoardName, boardName);
 }
-const changeTaskName = async (taskID, taskName) => {
-    return handleRequest(async () => {
-        return await serverRequest.changeTaskName(taskID, taskName);
-    });
+
+const changeTaskName = (taskID, taskName) => {
+    return handleRequest(serverRequest.changeTaskName, taskID, taskName);
 }
 
 export default {
